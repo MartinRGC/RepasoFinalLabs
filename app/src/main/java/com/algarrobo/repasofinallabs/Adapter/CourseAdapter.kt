36dca13efeeb5c834a014526d6bfe384bc2379db
Martin Garcia <martingarcia08.00@gmail.com>
2023-11-20 11:51:34 -0500

    lab autenticacion y visualizacion de datos de firebase y googlejson agregando el registro de curso y nota con listview1

--- app/src/main/java/com/algarrobo/repasofinallabs/Adapter/CourseAdapter.kt
+++ app/src/main/java/com/algarrobo/repasofinallabs/Adapter/CourseAdapter.kt
@@ -3,8 +3,6 @@ package com.algarrobo.repasofinallabs.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CursorAdapter
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.algarrobo.repasofinallabs.Models.CursoModel
