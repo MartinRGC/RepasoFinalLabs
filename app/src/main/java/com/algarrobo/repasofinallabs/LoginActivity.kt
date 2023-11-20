d8baf511ca1df6938696db992fce226895c5eb46
Martin Garcia <martingarcia08.00@gmail.com>
2023-11-20 09:46:14 -0500

    lab autenticacion y visualizacion de datos de firebase

---
+++ app/src/main/java/com/algarrobo/repasofinallabs/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.algarrobo.repasofinallabs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val edtemail: EditText= findViewById(R.id.edtemail)
+        val edtpss: EditText = findViewById(R.id.edtpss)
+        val btnlogin: Button = findViewById(R.id.btnlogin)
+
+        val db = FirebaseAuth.getInstance()
+
+
+        btnlogin.setOnClickListener {
+            // lo que escribe el usuario
+            var correo: String = edtemail.text.toString()
+            var clave: String = edtpss.text.toString()
+
+            db.signInWithEmailAndPassword(correo, clave)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content),
+                                "Inicio de sesión exitoso",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content),
+                                "Credenciales inválidas",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                    }
+                }
+        }
+
+    }
+}
--- app/src/main/java/com/algarrobo/repasofinallabs/MainActivity.kt
+++ app/src/main/java/com/algarrobo/repasofinallabs/MainActivity.kt
@@ -2,10 +2,49 @@ package com.algarrobo.repasofinallabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = FirebaseFirestore.getInstance()
+
+        val tvcurso: TextView = findViewById(R.id.tvCurso)
+        val tvnota: TextView = findViewById(R.id.tvNota)
+
+        // coleccion de firebase
+        // coleccion de firebase
+
+
+        db.collection("courses")
+            .addSnapshotListener{ snapshots, e ->
+                if(e != null){
+                  Log.w("Firebase", "Listen:error",e) // warning
+                    return@addSnapshotListener
+                }
+                for (dc in snapshots!!.documentChanges){
+                    when (dc.type) { // cuando sea del tipo
+                        DocumentChange.Type.ADDED ->{
+                            Log.d("Firebase", "Data: "+ dc.document.data)
+                            tvcurso.text = dc.document.data["description"].toString() // Data que se desea leer
+                            tvnota.text = dc.document.data["score"].toString() // Data que se desea leer
+                        }
+                        DocumentChange.Type.MODIFIED -> {
+                            tvcurso.text = dc.document.data["description"].toString()
+                            tvnota.text = dc.document.data["score"].toString()
+                        }
+                        DocumentChange.Type.REMOVED ->{
+                            tvcurso.text = "Curso eliminado"
+                            tvnota.text = "Nota eliminada"
+                        }
+
+                    }
+                }
+
+            }
     }
 }
